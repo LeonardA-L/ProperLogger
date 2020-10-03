f38083a53f4ea5e7e6924f6ab8b58a012f0ca762
Leonard <leonard.allainlaunay@gmail.com>
2020-10-03 12:46:28 +0200

    Add category filter window

---
+++ ProperConsole/CategoriesFilterGameWindow.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif //UNITY_EDITOR
+
+namespace ProperLogger
+{
+    internal class CategoriesFilterGameWindow : MonoBehaviour
+    {
+        private ConfigsProvider m_configs = null;
+
+        [SerializeField]
+        protected int m_depth = 1;
+
+        private void OnGUI()
+        {
+            if(ProperConsoleGameWindow.Instance == null)
+            {
+                return;
+            }
+
+            if (!ProperConsoleGameWindow.Instance.ShowCategoryFilter)
+            {
+                return;
+            }
+
+            if(ProperConsoleGameWindow.Instance.Skin != null)
+            {
+                GUI.skin = ProperConsoleGameWindow.Instance.Skin;
+            }
+
+            float refScreenWidth = 1280f;
+            float refScreenHeight = 768f;
+
+            float xFactor = Screen.width / refScreenWidth;
+            float yFactor = Screen.height / refScreenHeight;
+            GUIUtility.ScaleAroundPivot(new Vector2(xFactor, yFactor), Vector2.zero);
+
+            GUI.depth = m_depth;
+
+            GUILayout.BeginArea(ProperConsoleGameWindow.Instance.CategoryFilterRect, (GUIStyle)"Box");
+
+            m_configs = m_configs ?? new PlayerConfigs();
+
+            if(m_configs.CurrentCategoriesConfig == null)
+            {
+                GUILayout.Label("No categories asset have been configured.");
+
+                GUILayout.Space(15);
+            }
+            else
+            {
+                if(m_configs.CurrentCategoriesConfig.Categories == null || m_configs.CurrentCategoriesConfig.Categories.Count == 0)
+                {
+                    GUILayout.Label("No categories found.");
+                }
+                else
+                {
+                    Color defaultColor = GUI.color;
+                    var inactiveCategories = m_configs.InactiveCategories;
+                    foreach (var category in m_configs.CurrentCategoriesConfig.Categories)
+                    {
+                        bool lastActive = !inactiveCategories.Contains(category);
+                        GUI.color = Color.Lerp(category.Color, defaultColor, m_configs.CategoryNameColorize);
+                        bool isActive = GUILayout.Toggle(lastActive, category.Name);
+                        if (isActive != lastActive)
+                        {
+                            if (isActive)
+                            {
+                                inactiveCategories.Remove(category);
+                                m_configs.InactiveCategories = inactiveCategories;
+                            } else
+                            {
+                                inactiveCategories.Add(category);
+                                m_configs.InactiveCategories = inactiveCategories;
+                            }
+                        }
+                    }
+                    GUI.color = defaultColor;
+                }
+
+                GUILayout.Space(10);
+
+                if (GUI.changed)
+                {
+                    ProperConsoleGameWindow.Instance.TriggerFilteredEntryComputation();
+                }
+
+#if UNITY_EDITOR
+                if (GUILayout.Button("Open Categories Settings"))
+                {
+                    Selection.activeObject = m_configs.CurrentCategoriesConfig;
+                }
+#endif //UNITY_EDITOR
+            }
+            GUILayout.EndArea();
+
+            if((Event.current.type == EventType.ContextClick || Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseUp))
+            {
+                if (ProperConsoleGameWindow.Instance.CategoryFilterRect.Contains(Event.current.mousePosition))
+                {
+                    Event.current.Use();
+                }
+                else if(!ProperConsoleGameWindow.Instance.CategoryToggleRect.Contains(Event.current.mousePosition))
+                {
+                    ProperConsoleGameWindow.Instance.ShowCategoryFilter = false;
+                }
+            }
+        }
+
+    }
+}
